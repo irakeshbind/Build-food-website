f34172302e0e3254138849c4f3b05acbe89e84d1
irakeshbind <rakeshbind351998@gmail.com>
2024-07-25 22:41:02 +0530

    Add in the leaning text

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -6,7 +6,7 @@ return (
     <div className="content" id="obj">
         <h1>Evaluate Your Inner<br/> foodie with Every<br/> Bite.</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/> Aliquid, veritatis doloribus molestias mollitia <br/>laudantium molestiae fugit laborum possimus. Id, a!</p>
-        <button className='button-w'>Order now</button>
+        <button className='button-w'>learning</button>
         </div>
 <img src={img2} alt="" />
 
